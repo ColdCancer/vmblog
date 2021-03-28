f085e80d8d3c991682433f65a796f728cf98d514
ColdCancer <svipvm@yeah.net>
2021-03-28 14:20:22 +0800

    modify all files and add VisitorController file

--- src/main/java/com/demo/pojo/Blogger.java
+++ src/main/java/com/demo/pojo/Blogger.java
@@ -3,35 +3,17 @@ package com.demo.pojo;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.util.Date;
 
-//@Data
-//@AllArgsConstructor
-//@NoArgsConstructor
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Blogger {
     private String account;
     private String password;
-
-    public Blogger() {
-    }
-
-    public Blogger(String account, String password) {
-        this.account = account;
-        this.password = password;
-    }
-
-    public String getAccount() {
-        return account;
-    }
-
-    public void setAccount(String account) {
-        this.account = account;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    private String name;
+    private String sex;
+    private int age;
+    private String email;
+    private Date register;
 }
