c9ee2f76d1063179733293362fb2bbba56176e18
ColdCancer <svipvm@yeah.net>
2021-06-01 11:55:31 +0800

    add category controller

--- web/WEB-INF/static/js/admin-category.js
+++ web/WEB-INF/static/js/admin-category.js
@@ -8,41 +8,42 @@ $(function () {
         }
         return s;
     }
-    /*using ajax come to get tag.html content message*/
-    // $.ajax({
-    //     /*get json message by url*/
-    //     url: window.location.href + '/getTagList',
-    //     type: 'get',
-    //     dataType: 'json',
-    //     success: function (data) {
-    //         $('#tag-count').html(data.total);
-    //         $('#tag-link-count').html(data.linkCount);
-    //         for (var i = 0; i < data.tags.length; i++) {
-    //             var name = data.tags[i].name;
-    //             var count = data.tags[i].count;
-    //             var html = "<tr>\n<th scope=\"row\">{0}</th>\n<td>{1}</td>\n<td>{2}</td>\n" +
-    //                 "<td><a class=\"btn btn-warning\" href=\"{1}/updata\">更改</a></td>\n" +
-    //                 "<td><a class=\"btn btn-danger\" href=\"{1}/delete\">删除</a></td>\n</tr>";
-    //             html = String.format(html, i + 1, name, count);
-    //             $('#tag-list').append(html);
-    //         }
-    //     }
-    // });
 
-    $('#add-tag').click(function () {
+    /*using ajax come to get tag.html content message*/
+    $.ajax({
+        /*get json message by url*/
+        url: window.location.href + '/getCategoryList',
+        type: 'get',
+        dataType: 'json',
+        success: function (data) {
+            $('#category-count').html(data.total);
+            $('#category-link-count').html(data.linkCount);
+            for (var i = 0; i < data.categories.length; i++) {
+                var name = data.categories[i].name;
+                var count = data.categories[i].count;
+                var html = "<tr>\n<th scope=\"row\">{0}</th>\n<td>{1}</td>\n<td>{2}</td>\n" +
+                    "<td><a class=\"btn btn-warning\" href=\"{1}/updata\">更改</a></td>\n" +
+                    "<td><a class=\"btn btn-danger\" href=\"{1}/delete\">删除</a></td>\n</tr>";
+                html = String.format(html, i + 1, name, count);
+                $('#category-list').append(html);
+            }
+        }
+    });
+
+    $('#add-category').click(function () {
         // alert($('#tag-name').val());
         $.ajax({
             /*get json message by url*/
-            url: window.location.href + '/addTag',
-            data: {'tagName': $('#tag-name').val()},
+            url: window.location.href + '/addCategory',
+            data: {'categoryName': $('#category-name').val()},
             type: 'post',
             dataType: 'json',
             success: function (data) {
                 if (data.result) {
-                    alert("添加标签成功！");
+                    alert("添加类别成功！");
                     window.location.reload();
                 } else {
-                    alert("添加标签失败，检查是否已存在！");
+                    alert("添加类别失败，检查是否已存在！");
                 }
             }
         });
